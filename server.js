42d48a985596e1c605b8ea1d34a1b0545f9ef8ed
Mandip Tharu <mandip.chaudhary@yarshastudio.com>
2019-10-16 16:39:42 +0545

    basic hello world

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>')
+})
+
+app.listen(8000, () => console.log('Express server listening on port 8000'))
