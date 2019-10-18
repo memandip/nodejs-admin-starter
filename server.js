a06d08584a0a20f14abb03f1d07bc5415d2ce74e
Mandip Tharu <mandip.chaudhary@yarshastudio.com>
2019-10-18 12:04:38 +0545

    server listen port fix

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>')
 })
 
-app.listen(8000, () => console.log('Express server listening on port 8000'))
+app.listen(port, () => console.log('Express server listening on port 8000'))
